393ef607f6a42d3b64f960510037bc9fa488368a
olakaua <kakadzn@hotmail.com>
2023-08-09 21:32:11 -0300

    python basico

---
+++ Ciência de Dados Impressionador/Python Basico/tipos_de_dados.py
@@ -0,0 +1,11 @@
+faturamento = 1000
+print(type(faturamento))
+
+faturamento = 1000.00
+print(type(faturamento))
+
+faturamento = '1.000'
+print(type(faturamento))
+
+ganha_bonus = True
+print(type(ganha_bonus))
---
+++ Ciência de Dados Impressionador/Python Basico/variaveis.py
@@ -0,0 +1,5 @@
+qtde_vendas = 1800
+nome = 'Kauã'
+
+print(nome)
+print(qtde_vendas)
